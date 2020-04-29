1a95f6bb073d234dd10fcadfb83e6907a1c343d5
hooksha2020 <hookshamosiba201555@gmail.com>
2020-04-30 01:15:13 +0200

    finish

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -9,5 +9,7 @@ class UsersTableSeeder extends Seeder
     {
 		DB::table('governorates')->insert(['name' => 'الدقهلية', 'created_at' => now(), 'updated_at' => now()]);
 		DB::table('cites')->insert(['name' => 'المنصورة', 'created_at' => now(), 'updated_at' => now()]);
+		DB::table('fines')->insert(['license_number' => '1234', 'fine' => '2500', 'created_at' => now(), 'updated_at' => now()]);
+		DB::table('license_fees')->insert(['name' => 'test name', 'price' => '500', 'created_at' => now(), 'updated_at' => now()]);
     }
 }
