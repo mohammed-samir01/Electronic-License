9bb327a0c027296c1c7f26430d09e5809b62e141
hooksha2020 <hookshamosiba201555@gmail.com>
2020-04-30 00:57:53 +0200

    add the cities seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -8,5 +8,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 		DB::table('governorates')->insert(['name' => 'الدقهلية', 'created_at' => now(), 'updated_at' => now()]);
+		DB::table('cites')->insert(['name' => 'المنصورة', 'created_at' => now(), 'updated_at' => now()]);
     }
 }
