ee21f8b9b22f8aadf1e29014d02bc5a9cadcdf44
hooksha2020 <hookshamosiba201555@gmail.com>
2020-04-30 00:15:06 +0200

    add the seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+		DB::table('governorates')->insert(['name' => 'الدقهلية', 'created_at' => now(), 'updated_at' => now()]);
+    }
+}
