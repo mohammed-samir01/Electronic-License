be6e3c8137ee99721835cdbefd2ee8d1d930adea
hooksha2020 <hookshamosiba201555@gmail.com>
2020-04-30 01:19:32 +0200

    finsh

--- app/Http/Controllers/api/MainController.php
+++ app/Http/Controllers/api/MainController.php
@@ -24,7 +24,7 @@ class MainController extends Controller
 {
     public function contacts(Request $request){
 
-        $contacts = Contact::all();
+        $contacts = Contact::create($request->all());
          
         return responseJson(1 , 'successs' , $contacts);
     
